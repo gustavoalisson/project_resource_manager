0a25d090df0cd59edf3e8d246d575276f7f04b17
gustavoalisson <gustavoalisson112@gmail.com>
2023-07-16 00:51:27 -0300

    implementacao: busca de cep via api (via cep) / modificacao: padronizando código em apenas um idioma

--- process_purchase.py
+++ process_purchase.py
@@ -1,41 +1,41 @@
-def treat_client_request(stock, produto, quantidade, cliente, endereco, date_order, payment, pedidos_realizado):
+def treat_client_request(stock, product, amount, client, address, date_order, payment, completed_orders):
     
     # Verificar se o produto solicitado existe em estoque
     
-    if produto not in stock['Produto']:
-        print(f"O produto >> {produto} << não existe em nosso estoque.")
+    if product not in stock['Produto']:
+        print(f"O produto >> {product} << não existe em nosso estoque.")
         return
     
     # Verifica se a quantidade em estoque é suficiente
     
-    index = stock['Produto'].index(produto)
-    if quantidade > stock['Quantidade'][index]:
-        print(f"A quantidade solicitada do produto {produto} excede a disponibilidade atual. Em estoque: {stock['Quantidade'][index]}")
+    index = stock['Produto'].index(product)
+    if amount > stock['Quantidade'][index]:
+        print(f"A quantidade solicitada do produto {product} excede a disponibilidade atual. Em estoque: {stock['Quantidade'][index]}")
         return
     
     # Verificar se a quantidade está sendo passada como 0
-    if quantidade == 0:
+    if amount == 0:
         print('Não é possível passar o valor 0 para Realizar um pedido. ')
         return
     
-    print(f"Pedido recebido: {produto}, Quantidade: {quantidade}, Cliente: {cliente}")
+    print(f"Pedido recebido: {product}, Quantidade: {amount}, Cliente: {client}")
     
     
     pedido = {
-    'Cliente': cliente,     
-    'Produto': produto,
-    'Quantidade': quantidade,
-    'Endereço': endereco,
+    'Cliente': client,     
+    'Produto': product,
+    'Quantidade': amount,
+    'Endereço': address,
     'Data do Pedido': date_order,
     'Tipo_Pagamento': payment
         }
 
     # Só adiciona o pedido realizado a lista pedidos_realizado se a quantidade for igual ou abaixo do que tem disponível em estoque
     
-    if quantidade <= stock['Quantidade'][index]:
-        pedidos_realizado.append(pedido)     
+    if amount <= stock['Quantidade'][index]:
+        completed_orders.append(pedido)     
         # Atualize o estoque
-        stock['Quantidade'][index] -= quantidade
+        stock['Quantidade'][index] -= amount
     
     
     
--- screen.py
+++ screen.py
@@ -1,6 +1,8 @@
 import customtkinter
 import pandas as pd
+import requests
 import os
+import re
 
 from process_purchase import treat_client_request
 from stock_storage import stock
@@ -10,50 +12,84 @@ from tkinter import ttk
 # Ler configuração que tem em .env
 load_dotenv()
 
-pedidos_realizado = []
+completed_orders = []
 
-def realizar_pedido(product_var, quantity_var, client_var, address_var, date_order, payment_method_var):
-    produto = product_var.get()
-    quantidade = quantity_var.get()
-    cliente = client_var.get()
+def validate_zip(zip):
+    # Remove caracter que nao sao numéricos
+    zip = re.sub(r'\D', '', zip)
+
+    # Verificar se o CEP possui o formato correto
+    if re.match(r'^\d{8}$', zip):
+        return True
+    else:
+        return False
+
+def apply_mask_zip(zip):
+    # Remove caracter que nao sao numéricos
+    zip = re.sub(r'\D', '', zip)
+
+    # Aplicar a máscara
+    zip_formatted = re.sub(r'^(\d{5})(\d{3})$', r'\1-\2', zip)
+
+    return zip_formatted
+
+def get_address(zip_code_entry, label_address):
+    zip = zip_code_entry.get()
+         
+    if validate_zip(zip):
+        zip_formatted = apply_mask_zip(zip)
+        url = f"{os.getenv('BASE_URL_VIACEP')}{zip_formatted}/json/"
+        response = requests.get(url)
+        data = response.json()
+        if "erro" not in data:
+            label_address.set(data["logradouro"])
+        print('CEP válido')
+    else:
+        print('CEP inválido! ')
+    
+def execute_request(product_var, quantity_var, client_var, address_var, date_order, payment_method_var):
+    product = product_var.get()
+    amount = quantity_var.get()
+    client = client_var.get()
     address = address_var.get()
     date = date_order.get()
     payment = payment_method_var.get()
     
     # Condição que obriga o User passar todos os dados
-    if produto and quantidade and cliente and address and date and payment:
+    if product and amount and client and address and date and payment:
   
-        if quantidade.isdigit():
+        if amount.isdigit():
             
-            quantidade = int(quantidade)
+            amount = int(amount)
         
-            treat_client_request(stock, produto, quantidade, cliente, address, date, payment, pedidos_realizado)
+            treat_client_request(stock, product, amount, client, address, date, payment, completed_orders)
         else:
             print('Digite um valor válido em >>> quantidade <<< ')
         
     else:
         print('Dados incompletos. Preencha todos os campos.')
 
-def finalizar_pedido():  
+def finalize_order():  
     
     print('Pedido finalizado com sucesso! ')
     
     # Gerando relatório em csv com os dados dos pedidos dos Clientes
-    df_pedidos = pd.DataFrame(pedidos_realizado)
+    df_pedidos = pd.DataFrame(completed_orders)
     df_pedidos.to_csv(os.getenv('PATH_REPORT'))
     
     return df_pedidos
 
 # * Usando o customtkinter/tkinter  para gerar uma tela e simular o lado do Cliente / "Compra do Produto"
 def generate_screen():
-
-    
+ 
     customtkinter.set_appearance_mode("System")
     customtkinter.set_default_color_theme("blue")
 
 
     # Instancia para iniciar uma nova janela
     window = customtkinter.CTk()
+    
+    # window.attributes('-fullscreen',True)
 
     tree = ttk.Treeview(window, columns=list(stock.keys()), show='headings')
 
@@ -70,9 +106,12 @@ def generate_screen():
 
     window.iconbitmap(os.getenv('ICON_MESHA_APP'))
 
-    window.geometry("800x650")
+    window.geometry("1200x800")
 
     window.title('Registro de Pedido')
+
+    
+    #declaracao de variaveis 
 
     client_var = customtkinter.StringVar()
     address_var = customtkinter.StringVar()
@@ -80,12 +119,21 @@ def generate_screen():
     product_var = customtkinter.StringVar()
     quantity_var = customtkinter.StringVar()
     payment_method_var = customtkinter.StringVar()
-
+    zip = customtkinter.StringVar() #novo
+    
+    
     label_client = customtkinter.CTkLabel(window, text="Cliente:")
     entry_client = customtkinter.CTkEntry(window, textvariable=client_var, width=300, height=25)
+    
+    
+    label_zip = customtkinter.CTkLabel(window, text="CEP: ") #novo
+    entry_zip  = customtkinter.CTkEntry(window, textvariable=zip)
+    botao_buscar = customtkinter.CTkButton(window, text='Buscar', command=lambda: get_address(zip, address_var))
 
-    label_address = customtkinter.CTkLabel(window, text="Endereço de Entrega:")
+        
+    label_address = customtkinter.CTkLabel(window, text="Logradouro")
     entry_address = customtkinter.CTkEntry(window, textvariable=address_var, width=300, height=25)
+    
 
     label_date_order = customtkinter.CTkLabel(window, text="Data do Pedido:")
     entry_date = customtkinter.CTkEntry(window, textvariable=date_order, width=300, height=25)
@@ -102,17 +150,21 @@ def generate_screen():
 
     # Botão de processar pedido
     button_process = customtkinter.CTkButton(window, text="Registrar Pedido",
-                                        command=lambda: realizar_pedido(product_var, quantity_var, client_var, address_var, date_order, payment_method_var))
+                                        command=lambda: execute_request(product_var, quantity_var, client_var, address_var, date_order, payment_method_var))
 
 
     # Botão para finalizar o pedido
     button_finalize = customtkinter.CTkButton(window, text="Finalizar Pedido",
-                                          command=lambda: finalizar_pedido(), fg_color="red")
+                                          command=lambda: finalize_order(), fg_color="red")
 
     # Posicionar os elementos na janela
     label_client.pack()
     entry_client.pack()
 
+    label_zip.pack()
+    entry_zip.pack()
+    botao_buscar.pack()
+    
     label_address.pack()
     entry_address.pack()
 
@@ -128,10 +180,11 @@ def generate_screen():
     label_payment_method.pack()
     entry_payment_method.pack()
 
-    button_process.pack(pady=12,padx=10)
+    button_process.pack(pady=3,padx=5)
 
-    button_finalize.pack(pady=12,padx=10)
+    button_finalize.pack()
 
+    # mantem a tela aberta
     window.mainloop()
 
 
