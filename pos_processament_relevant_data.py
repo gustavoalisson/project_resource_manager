f89650a1aa759e07ee2b5c94a53b8df355f010f4
gustavoalisson <gustavoalisson112@gmail.com>
2023-07-16 16:33:39 -0300

    removendo comentário de teste

--- pos_processament_relevant_data.py
+++ pos_processament_relevant_data.py
@@ -47,42 +47,6 @@ insert_all_orders_columns()
 
 
 
-# import pandas as pd
-# import sqlite3
-
-# # Carregar o arquivo CSV
-# df = pd.read_csv('relatorio_pedido_realizado.csv')
-
-# # Filtrar as colunas relevantes
-# df_filtrado = df[['Produto', 'Quantidade']]
-
-# # Calcular a soma da coluna 'Quantidade' para cada produto
-# df_produtos = df_filtrado.groupby('Produto')['Quantidade'].sum().reset_index()
-
-# # Identificar o produto mais comprado
-# produto_mais_comprado = df_produtos[df_produtos['Quantidade'] == df_produtos['Quantidade'].max()]
-
-# # Conectar ao banco de dados SQLite
-# con = sqlite3.connect('DB_MESHA_MANAGER.db')
-# cursor = con.cursor()
-
-# # Criar a tabela no banco de dados (caso ainda não exista)
-# cursor.execute('''CREATE TABLE IF NOT EXISTS produto_mais_comprado (
-#                     Produto TEXT,
-#                     Quantidade INTEGER
-#                     )''')
-
-# # Inserir o produto mais comprado no banco de dados
-# produto_mais_comprado.to_sql('produto_mais_comprado', con, if_exists='replace', index=False)
-
-# # Fechar a conexão com o banco de dados
-# con.close()
-
-
-
-
-
-
 
 
 
