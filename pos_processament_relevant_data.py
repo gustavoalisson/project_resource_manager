1c1174d0758b491049bd76e256cb4fba98f655f4
gustavoalisson <gustavoalisson112@gmail.com>
2023-07-17 09:37:52 -0300

    implementacao de dado relevante: dia que tem mais compra / mes que tem mais compra

--- pos_processament_relevant_data.py
+++ pos_processament_relevant_data.py
@@ -7,13 +7,13 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-def connection_database():
+def connection_database(logger):
     try:        
         conn = sqlite3.connect(os.getenv('NAME_DB'))
-        print('Conexão bem-sucedida!')
+        logger.info('Conexão bem-sucedida!')
         return conn
     except Exception as e:
-        print(f'Erro ao conectar com o banco de dados {e}')
+        logger.error(f'Erro ao conectar com o banco de dados {e}')
         raise
     
 def read_csv_orders():
@@ -31,7 +31,7 @@ def filter_all_columns(df):
     return df_filtered
 
 # Insere todas as colunas gerada no arquivo CSV no database
-def insert_all_orders_columns(df, conn):
+def insert_all_orders_columns(df, conn, logger):
     
     df_filtered = filter_all_columns(df)
     
@@ -48,7 +48,7 @@ def insert_all_orders_columns(df, conn):
 
     df_filtered.to_sql('pedidos', conn, if_exists='append', index=False)
     
-    print('Dados do relatório inserido na base de dados com sucesso!!!')
+    logger.info('Dados do relatório inserido na base de dados com sucesso!!!')
     
 
 def filter_column_quantity_and_product(df):
@@ -57,7 +57,7 @@ def filter_column_quantity_and_product(df):
     return df_filtered
 
 # Insere em uma tabela específica o produto mais comprado
-def insert_most_purchased_product(df, conn):
+def insert_most_purchased_product(df, conn, logger):
     df_filtered = filter_column_quantity_and_product(df)
     
     cursor = conn.cursor()
@@ -73,7 +73,7 @@ def insert_most_purchased_product(df, conn):
 
     product_most_bought.to_sql('produto_mais_comprado', conn, if_exists='replace', index=False)
     
-    print('Dados de produto mais comprado inserido com sucesso!')
+    logger.info('Dados de produto mais comprado inserido com sucesso!')
 
   
 def filter_column_client_and_product(df):
@@ -82,7 +82,7 @@ def filter_column_client_and_product(df):
     return df_filtered
     
 # Insere em uma tabela específica um produto mais popular para cada cliente
-def product_most_popular_by_customer(df, conn):
+def product_most_popular_by_customer(df, conn, logger):
     df_filtered = filter_column_client_and_product(df)
     
     cursor = conn.cursor()
@@ -99,34 +99,49 @@ def product_most_popular_by_customer(df, conn):
 
     popular_products_by_customer.to_sql('produtos_populares_por_cliente', conn, if_exists='replace', index=False)
     
-    print('Dados de produto mais popular por cliente, inserido com sucesso! ')
+    logger.info('Dados de produto mais popular por cliente, inserido com sucesso! ')
     
+# Identifica padrão de vende = Quais dias tem mais compras
+def orders_by_day_of_week(df, conn, logger):
+    df['Data_do_Pedido'] = pd.to_datetime(df['Data_do_Pedido'])
+    df['Dia_da_Semana'] = df['Data_do_Pedido'].dt.day_name()
+    
+    orders_by_day_week = df['Dia_da_Semana'].value_counts().reset_index()
+    orders_by_day_week.columns = ['Dia_da_Semana', 'Total_Pedidos']
+    
+    orders_by_day_week.to_sql('pedidos_por_dia_semana', conn, if_exists='replace', index=False)
+    
+    logger.info('Dados de pedidos por dia da semana inserido com sucesso!')
+    
+# Identifica padrão de vende = Quais meses tem mais compra    
+def orders_by_month(df, conn, logger):
+    df['Data_do_Pedido'] = pd.to_datetime(df['Data_do_Pedido'])
+    df['Mes'] = df['Data_do_Pedido'].dt.month_name()
+    
+    orders_per_month = df['Mes'].value_counts().reset_index()
+    orders_per_month.columns = ['Mês', 'Total_Pedidos']
+    
+    orders_per_month.to_sql('pedidos_por_mes', conn, if_exists='replace', index=False)
+    
+    logger.info('Dados de pedidos por mês inserido com sucesso!')
+        
 
-
-def insert_relevant_information_into_database():
+def insert_relevant_information_into_database(logger):
      
     df = read_csv_orders()
     
     if df is None:
-        print('Arquivo CSV não foi encontrado. Não é possível inserir os dados relevantes ao banco de dados.')
+        logger.warning('Arquivo CSV não foi encontrado. Não é possível inserir os dados relevantes ao banco de dados.')
     else:
-        conn = connection_database()
+        conn = connection_database(logger)
         
-        insert_all_orders_columns(df, conn) 
-        insert_most_purchased_product(df, conn)
-        product_most_popular_by_customer(df, conn)
+        insert_all_orders_columns(df, conn, logger) 
+        insert_most_purchased_product(df, conn, logger)
+        product_most_popular_by_customer(df, conn, logger)
+        orders_by_day_of_week(df, conn, logger)
+        orders_by_month(df, conn, logger)
         
         conn.close()
     
-
-
-
-
-
-
-
-
-
-
 
 
